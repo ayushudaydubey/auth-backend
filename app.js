673dac5d0942ce6bfe0145c85a920332018f1f53
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-13 17:03:04 +0530

    url

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db.js');
 const authRoutes = require('./routes/authRoutes.js');
-const userRoutes = require("./routes/userRoutes.js");
+const userRoutes = require('./routes/userRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://auth-frontend-orcin.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
